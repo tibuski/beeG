59c2e41805200c3aa0c5f95bae85cf090f74d960
tibuski <laurent.brichet@gmail.com>
2022-12-03 16:57:07 +0100

    firt post worked

--- beeG.go
+++ beeG.go
@@ -1,31 +1,35 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
 type Options struct {
-	AUTHKEY        string
-	TEAMID         string
-	REVISION       string
-	HEARTBEAT      string
-	ZOMBEAT        string
-	X_ORG_ID       string
-	CMDB_SERVICE   string
-	ALERTID1       string
-	ALERTID2       string
-	ALERTTEXT      string
-	ALERTENV       string
-	BATCH_LOCATION string
-	BATCH_COMMENT  string
-	BATCH_URL      string
-	BATCH_DOC      string
-	BEHAVIOR       string
-	ALERTMAIL      string
-	BEE_URL        string
+	AUTHKEY          string
+	EFFECTIVE_STATUS string
+	TEAMID           string
+	REVISION         string
+	HEARTBEAT        string
+	ZOMBEAT          string
+	VALUE            string
+	X_ORG_ID         string
+	CMDB_SERVICE     string
+	ALERTID1         string
+	ALERTID2         string
+	ALERTTEXT        string
+	ALERTENV         string
+	BATCH_LOCATION   string
+	BATCH_COMMENT    string
+	BATCH_URL        string
+	BATCH_DOC        string
+	BEHAVIOR         string
+	ALERT_MAIL       string
+	BEE_URL          string
 }
 
 func prettyPrint(i interface{}) string {
@@ -54,7 +58,27 @@ func getOptionsFromIniFile(f string) Options {
 	scriptPath, _ := os.Executable()
 	conf.BATCH_LOCATION = hostName + " - " + scriptPath
 
+	// Get status from ARG2
+	conf.EFFECTIVE_STATUS = os.Args[2]
+
+	// Get Value from ARG3
+	conf.VALUE = os.Args[3]
+
 	return conf
+}
+
+func postToBee(c Options) string {
+	url := c.BEE_URL
+	msg, err := json.Marshal(c)
+	if err != nil {
+		log.Fatalf("Convert to JSON error :\n %v", err)
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(msg))
+	if err != nil {
+		log.Fatalf("HTTP POST error :\n %v", err)
+	}
+	fmt.Printf("%v\n", resp)
+	return ("ok")
 }
 
 func main() {
@@ -63,6 +87,9 @@ func main() {
 
 	// Get Options from ini file
 	config := getOptionsFromIniFile(iniFile)
+
+	// Send Data to BEE URL
+	postToBee(config)
 
 	// Pretty Print content of "config"
 	fmt.Println(prettyPrint(config))
