bfa10eeeda550994d7e9f19c60c7c760ff3bee2a
tibuski <laurent.brichet@gmail.com>
2022-12-03 14:15:01 +0100

    json to options ok

--- beeG.go
+++ beeG.go
@@ -3,24 +3,67 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 )
 
-func main() {
+type Options struct {
+	AUTHKEY        string
+	TEAMID         string
+	REVISION       string
+	HEARTBEAT      string
+	ZOMBEAT        string
+	X_ORG_ID       string
+	CMDB_SERVICE   string
+	ALERTID1       string
+	ALERTID2       string
+	ALERTTEXT      string
+	ALERTENV       string
+	BATCH_LOCATION string
+	BATCH_COMMENT  string
+	BATCH_URL      string
+	BATCH_DOC      string
+	BEHAVIOR       string
+	ALERTMAIL      string
+	BEE_URL        string
+}
 
-	jsonFile, err := os.Open("beex.ini")
+func prettyPrint(i interface{}) string {
+	// Function : Pretty print config array
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func getOptionsFromIniFile(f string) Options {
+	// Function : read and format ini file
+	conf := Options{}
+
+	// Read iniFile and print error if any
+	jsonContent, err := os.ReadFile(f)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+	}
+	// Read JSON data in variable "content" in struct "config"
+	err = json.Unmarshal(jsonContent, &conf)
+	if err != nil {
+		log.Fatalf("JSON file error :\n %v", err)
 	}
 
-	fmt.Println("Successfully opened bee.ini")
-	defer jsonFile.Close()
+	// Create Batch Location string
+	hostName, _ := os.Hostname()
+	scriptPath, _ := os.Executable()
+	conf.BATCH_LOCATION = hostName + " - " + scriptPath
 
-	byteResult, _ := ioutil.ReadAll(jsonFile)
+	return conf
+}
 
-	var res map[string]interface{}
-	json.Unmarshal([]byte(byteResult), &res)
+func main() {
 
-	fmt.Println(res["options"])
+	iniFile := os.Args[1]
+
+	// Get Options from ini file
+	config := getOptionsFromIniFile(iniFile)
+
+	// Pretty Print content of "config"
+	fmt.Println(prettyPrint(config))
 }
